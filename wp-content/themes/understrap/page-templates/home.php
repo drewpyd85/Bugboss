7ee8e51650bdf69f0fe7d86d7c2277d65a4c8c56
Drew Ramasco <drewpyd85@gmail.com>
2017-10-17 19:58:50 -0400

    updates to bootstrap

--- wp-content/themes/understrap/page-templates/home.php
+++ wp-content/themes/understrap/page-templates/home.php
@@ -8,26 +8,13 @@
  */
 
 get_header();
+
+
+
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="full-width-page-wrapper">
-    
-    <div class="jumbotron jumbotron-fluid home-jumbotron">
-    
-        <div class="container">
-            <h1 class="display-3">The<br> Bug<br> Boss</h1>
-            <p class="lead">
-            Contract killing pest for 25 years
-            </p>
-            <a href="#" class="cta-hero rounded">Call Now</a>
-        </div>
-        
-    </div>
-    
+<?php include 'inc/home-hero.php'; ?>
 
-</div><!-- Wrapper end -->
-
-<!--<div class="spacer"></div>-->
 
 <?php get_footer(); ?>
