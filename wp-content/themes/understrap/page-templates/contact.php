013236318e5172ef0f7d0c02fdfaf54b43eae6e3
Drew Ramasco <drewpyd85@gmail.com>
2017-11-17 08:06:34 -0500

    final touch ups

--- wp-content/themes/understrap/page-templates/contact.php
+++ wp-content/themes/understrap/page-templates/contact.php
@@ -19,10 +19,11 @@ $container = get_theme_mod( 'understrap_container_type' );
     <div class="container py-5">
         <div class="row d-flex align-items-center">
             <div class="col-md-6">
+                <h2 class="text-center">Stop Pest Now</h2>
                 <?php include 'inc/partials/contact-form.php'; ?>
             </div>
             
-            <div class="col-md-6">
+            <div class="col-md-6 mt-3">
               
                <h1>The Bug Boss</h1>
                <p class="lead">We'll Be Happy To Answer Any Questions</p>
@@ -33,7 +34,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                     <li>The Bug Boss, LLC</li>
                     <li><a href="tel:3522222222"> Phone</a></li>
                     <li><a href="mailto:email.com"> Email</a></li>
-                    <li> 43 Teak Run<br> Ocala, FL 34472</li>
+                    <li> 43 Teak Run, Ocala, FL 34472</li>
                 </ul>
                 
             </div>
@@ -46,7 +47,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
     <div class="row">
        
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3483.9088462328527!2d-82.04951038396489!3d29.16735828220998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7d2a5cb06564b%3A0x1f6a0b655ad537d4!2s43+Teak+Run%2C+Ocala%2C+FL+34472!5e0!3m2!1sen!2sus!4v1510340891899" width="2560" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16572.453286226977!2d-82.05924674251207!3d29.166760302840135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7d2a5cb06564b%3A0x1f6a0b655ad537d4!2s43+Teak+Run%2C+Ocala%2C+FL+34472!5e0!3m2!1sen!2sus!4v1510923912890" width="2560" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 
     </div>
 
