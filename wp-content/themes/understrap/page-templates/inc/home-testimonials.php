f1ace00bed21da6854b5ffaf517aaa613ced12c6
Drew Ramasco <drewpyd85@gmail.com>
2017-11-29 11:35:46 -0500

    added duplicator plugin

--- wp-content/themes/understrap/page-templates/inc/home-testimonials.php
+++ wp-content/themes/understrap/page-templates/inc/home-testimonials.php
@@ -17,20 +17,24 @@
             
         <div class="testimonials">
             <div class="test-slide text-center">
-                <img src="http://fillmurray.com/g/200/200" alt="The Bug Boss Testimonial" class="avatar">
-                <span class="h4">Name</span><br>   
-                <span class="h6">Position</span> 
-                <p><span class="quote"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quod ab nisi aliquam? Necessitatibus ea, culpa mollitia, fugiat aut eligendi, minima sit, possimus doloribus debitis fuga placeat optio exercitationem reprehenderit?<span class="quote2"></span></p>
+<!--                <img src="http://fillmurray.com/g/150/150" alt="The Bug Boss Testimonial" class="avatar">-->
+                <span class="h4">Sharon Scott</span><br>   
+                <span class="h6">Nurse</span> 
+                <p><span class="quote"></span>The Bug Boss took care of us when we needed him. We're always having family over and when we found roaches, we needed them dead FAST. I would recommend them to all my friends and family.<span class="quote2"></span></p>
             </div>
              
-            <div class="test-slide">
-                <img src="http://fillmurray.com/g/200/200" alt="The Bug Boss Testimonial" class="avatar">        
-                <p><span class="quote"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia neque laborum dolores vitae pariatur amet ipsam id unde quis, cupiditate consequatur, velit iste ad deserunt repellat repellendus minima soluta iusto.<span class="quote2"></span></p>
+            <div class="test-slide text-center">
+<!--                <img src="http://fillmurray.com/g/150/150" alt="The Bug Boss Testimonial" class="avatar">-->
+                <span class="h4">Lydia Albano</span><br>   
+                <span class="h6">Veterinarian</span>         
+                <p><span class="quote"></span>I work with animals on a daily basis so flea control is a "must" in my office. The Bug Boss keeps our office and outside area free of insects so our patients don't accidently pick up any unwanted pest.<span class="quote2"></span></p>
             </div>
               
-            <div class="test-slide">
-                <img src="http://fillmurray.com/g/200/200" alt="The Bug Boss Testimonial" class="avatar">
-                <p><span class="quote"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est perspiciatis maxime voluptate, iure corrupti. Dolorum rem quia distinctio voluptate illum, odit? Eum ad modi, doloribus commodi maiores eligendi doloremque.<span class="quote2"></span></p>
+            <div class="test-slide text-center">
+<!--                <img src="http://fillmurray.com/g/150/150" alt="The Bug Boss Testimonial" class="avatar">-->
+                <span class="h4">Amelia Batista</span><br>   
+                <span class="h6">Physician</span> 
+                <p><span class="quote"></span>We didn't know we had a roach problem until we saw a couple in our kitchen. The Bug Boss showed us how roaches can hide in cabinets and behind walls and what to do to prevent them. The technician got our bug problem under control in no time.<span class="quote2"></span></p>
             </div>
         </div>
         
